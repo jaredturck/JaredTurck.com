880b3c5204fd6aff5f63903e7807392842fac331
Jared Turck <jaredturck8@gmail.com>
2026-01-27 03:27:32 +0000

    Updated vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import prerender from 'vite-plugin-prerender'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
     prerender({
-      staticDir: 'dist',
+      staticDir: path.resolve(process.cwd(), 'dist'),
       routes: ['/', '/titusai'],
     }),
   ],
